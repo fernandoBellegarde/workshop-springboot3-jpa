12ae96a43d4f92caed1e55e202e4f67daa93140b
Fernando Bellegarde <bellegardefernando@gmail,com>
2025-03-26 18:06:52 -0300

    Order status enum

--- course/src/main/java/com/educandoweb/course/config/TestConfig.java
+++ course/src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.config;
 
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +32,12 @@ public class TestConfig implements CommandLineRunner {
         User u4 = new User(null, "Allan Grey", "allan@gmail.com", "955555555", "123456");
         User u5 = new User(null, "Joseph Blue", "joseph@gmail.com", "944444444", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
-        Order o4 = new Order(null, Instant.parse("2019-07-23T10:10:33Z"), u3);
-        Order o5 = new Order(null, Instant.parse("2006-10-02T23:06:06Z"), u3);
-        Order o6 = new Order(null, Instant.parse("2019-07-24T10:10:33Z"), u4);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o4 = new Order(null, Instant.parse("2019-07-23T10:10:33Z"), OrderStatus.CANCELED, u3);
+        Order o5 = new Order(null, Instant.parse("2006-10-02T23:06:06Z"), OrderStatus.DELIVERED, u3);
+        Order o6 = new Order(null, Instant.parse("2019-07-24T10:10:33Z"), OrderStatus.SHIPPED, u4);
 
         userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6));
